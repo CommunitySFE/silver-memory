e37fdd50c317de06dba59341b072bb793da447b4
skyefoxie <actuallyori@gmail.com>
2020-06-16 02:17:46 +0100

    remove unused code blocks in error_handler

--- structures/utilbot.py
+++ structures/utilbot.py
@@ -60,9 +60,11 @@ class UtilBot(commands.Bot):
         self.logger.info('------')
 
     async def on_command_error(self, ctx, error):
-        """The event triggered when an error is raised while invoking a command.
+        """
+        The event triggered when an error is raised while invoking a command.
         ctx   : Context
-        error : Exception"""
+        error : Exception
+        """
 
         # This prevents any commands with local handlers being handled here in on_command_error.
         if hasattr(ctx.command, 'on_error'):
@@ -89,12 +91,9 @@ class UtilBot(commands.Bot):
                 pass
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            # await ctx.message.add_reaction('❌')
-            # @callidus is this a bad idea to do?
             return await ctx.send(f":x: {ctx.command} is missing required argument {error.param}.")
 
         elif isinstance(error, commands.BadArgument):
-            # await ctx.message.add_reaction('❌')
             return await ctx.send(f":x: {ctx.command} {error}")
 
         elif isinstance(error, commands.DisabledCommand):
@@ -105,12 +104,6 @@ class UtilBot(commands.Bot):
                 return await ctx.author.send(f':x: {ctx.command} can not be used in Private Messages.')
             except Exception:
                 pass
-
-        # commented out because I am not sure what it does
-        # # For this error example we check to see where it came from...
-        # elif isinstance(error, commands.BadArgument):
-        #     if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
-        #         return await ctx.send(':x: I could not find that member. Please try again.')
 
         elif isinstance(error, commands.NotOwner):
             return await ctx.send(error)
@@ -124,33 +117,12 @@ class UtilBot(commands.Bot):
         )
         self.logger.exception(error)
 
-        channel = self.get_channel(623247355575009331)
-        if channel:
-            await channel.send(
-                f"```py\n{error[-1950:]}\n```"
-            )
-
-    # """Below is an example of a Local Error Handler for our command do_repeat"""
-    # @commands.command(name='repeat', aliases=['mimic', 'copy'])
-    # async def do_repeat(self, ctx, *, inp: str):
-    #     """A simple command which repeats your input!
-    #     inp  : The input to be repeated"""
-
-    #     await ctx.send(inp)
-
-    # @do_repeat.error
-    # async def do_repeat_handler(self, ctx, error):
-    #     """A local Error Handler for our command do_repeat.
-    #     This will only listen for errors in do_repeat.
-
-    #     The global on_command_error will still be invoked after."""
-
-    #     # Check if our required argument inp is missing.
-    #     if isinstance(error, commands.MissingRequiredArgument):
-    #         if error.param.name == 'inp':
-    #             await ctx.send("You forgot to give me input to repeat!")
 
 def signal_handler(sig, frame):
+    """
+    Gracefully shut down the bot when SIGINT signal is received. Not sure if this actually
+    makes a difference lol.
+    """
     bot.logger.info("Exit request received - shutting down...")
     run_coroutine_threadsafe(bot.logout(), bot.loop)
     exit(0)
